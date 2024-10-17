daa72a44eec61f11f2bae044f97e081ad48fc0e6
shulabh234 <shulabhdixit234@gmail.com>
2024-10-17 23:42:42 +0530

    commit all test cases and calc module

--- calc.py
+++ calc.py
@@ -4,3 +4,11 @@ def add(x: int, y: int) -> int:
 
 def sub(x: int, y: int) -> int:
     return abs(x - y)
+
+
+def mul(x: int, y: int) -> int:
+    return abs(x * y)
+
+
+def div(x: int, y: int) -> int:
+    return x / y
--- test_calc.py
+++ test_calc.py
@@ -8,3 +8,11 @@ class TestCalculator(unittest.TestCase):
 
     def test_sub(self):
         self.assertEqual(calc.sub(10, 20), 10)
+
+    def test_mul(self):
+        self.assertEqual(calc.mul(10, 10), 100)
+
+    def test_div(self):
+        self.assertEqual(calc.div(10, 2), 5)
+        with self.assertRaises(Exception):
+            self.assertEqual(calc.div(10, 0), 0)
